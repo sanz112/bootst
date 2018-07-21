7a9c8ff98d4cafca09e23c978dd41405ab229ae0
samuel <samueljohn498@gmail.com>
2018-07-21 01:01:23 +0100

    new changes

--- index.php
+++ index.php
@@ -26,6 +26,13 @@
     <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
     <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
     <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+
+
 
     <!-- Custom styles for this template -->
     <link href="css/resume.min.css" rel="stylesheet">
@@ -295,24 +302,24 @@
        <footer>
       <div style="font-size: 12px;" class="row text-warning" id="footer text-center ">
 
-        <div class="col-md-4"> &copy; Copyright 2018. Startbootstrap</div>
-		 <div class="col-md-4">Powered by <strong>Witt Technologies</strong></div>
-		  <div class="col-md-4"> 
-		  <div class="col-md-6" title="Nothing is here yet. More updates coming soon.">
+        <div class="col-md-3"> &copy; Copyright 2018. Startbootstrap</div>
+		 <div class="col-md-3">Powered by <strong>Witt Technologies</strong></div>
+		  
+		  <div class="col-md-3" title="Nothing is here yet. More updates coming soon.">
 		  Privacy Policy
 		  </div>
-		  <div class="col-md-6" title="Nothing is here yet. More updates coming soon.">
+		  <div class="col-md-3" title="Nothing is here yet. More updates coming soon.">
 		  Terms and Conditions
 		  </div>
       </div>
-	  </div>
+	  
     </footer>
     </section>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>-->
     
 
     <!-- Plugin JavaScript -->
